4018e4df2e81eb19f2068dbc12d8a8c3e5949719
Silvio Queiroz <silviosouza1207@gmail.com>
2024-08-28 20:22:11 -0300

    Initial Commit

---
+++ DotNet.RabbitMQ.Envia/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+//Crio uma factory de conexão com nome do host
+var factory = new ConnectionFactory { HostName = "localhost" }; 
+
+using var connection = factory.CreateConnection(); // Crio a conexão
+using var channel = connection.CreateModel(); // Crio um canal
+
+//Crio uma fila no meu canal com nome de hello
+channel.QueueDeclare(queue: "hello",
+                     durable: false, 
+                     exclusive: false, 
+                     autoDelete: false, 
+                     arguments: null);
+
+//Console.WriteLine("Digite sua mensagem");
+
+//while (true)
+//{
+    //string? message = Console.ReadLine();
+
+    //if (string.IsNullOrEmpty(message))
+    //    break;
+
+    var aluno = new Aluno() { Id = 1, Name = "Silvio" };
+    string message = JsonSerializer.Serialize<Aluno>(aluno);
+    
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey:"hello", 
+                         basicProperties: null,
+                         body: body);
+
+    Console.WriteLine("Mensagem enviada");
+    Console.ReadKey();
+//}
+
+class Aluno
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
